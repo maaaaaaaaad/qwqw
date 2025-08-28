1a0a51c9a944332ae21959bb18c9052c2225756c
maaaaaaaaad <integral5137@gmail.com>
2025-08-28 15:41:08 +0900

    Add NicknameTest to validate Nickname domain behaviors and constraints

---
+++ src/test/kotlin/com/mad/jellomarkserver/domain/model/NicknameTest.kt
@@ -0,0 +1,48 @@
+package com.mad.jellomarkserver.domain.model
+
+import com.mad.jellomarkserver.domain.model.member.Nickname
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class NicknameTest {
+    @Test
+    fun `Accept minimum length 2`() {
+        val n = Nickname.of("ab")
+        assertEquals("ab", n.value)
+    }
+
+    @Test
+    fun `Accept maximum length 8`() {
+        val n = Nickname.of("abcdefgh")
+        assertEquals("abcdefgh", n.value)
+    }
+
+    @Test
+    fun `Valid after trimming`() {
+        val n = Nickname.of("  ab  ")
+        assertEquals("ab", n.value)
+    }
+
+    @Test
+    fun `Reject length 1`() {
+        assertFailsWith<IllegalArgumentException> { Nickname.of("a") }
+    }
+
+    @Test
+    fun `Reject length 9`() {
+        assertFailsWith<IllegalArgumentException> { Nickname.of("abcdefghi") }
+    }
+
+    @Test
+    fun `Reject if contains whitespace`() {
+        assertFailsWith<IllegalArgumentException> { Nickname.of("a b") }
+    }
+
+    @Test
+    fun `Reject null and blank`() {
+        assertFailsWith<IllegalArgumentException> { Nickname.of(null) }
+        assertFailsWith<IllegalArgumentException> { Nickname.of("") }
+        assertFailsWith<IllegalArgumentException> { Nickname.of("   ") }
+    }
+}
