81db83a506f7f44d25867c7fad099fb1d2ad0c18
maaaaaaaaad <integral5137@gmail.com>
2025-09-15 19:22:00 +0900

    Remove unused MemberRepository interface from the Member domain

--- src/main/kotlin/com/mad/jellomarkserver/member/port/driven/MemberRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.mad.jellomarkserver.member.port.driven
-
-import com.mad.jellomarkserver.member.adapter.out.persistence.entity.MemberJpaEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface MemberRepository : JpaRepository<MemberJpaEntity, UUID>
