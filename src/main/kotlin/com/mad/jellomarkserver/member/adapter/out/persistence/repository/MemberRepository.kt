9f06bc6358199686bf196916d47b785c01afb1e2
maaaaaaaaad <integral5137@gmail.com>
2025-09-15 19:22:05 +0900

    Add MemberRepository interface for Member JPA persistence

---
+++ src/main/kotlin/com/mad/jellomarkserver/member/adapter/out/persistence/repository/MemberRepository.kt
@@ -0,0 +1,7 @@
+package com.mad.jellomarkserver.member.adapter.out.persistence.repository
+
+import com.mad.jellomarkserver.member.adapter.out.persistence.entity.MemberJpaEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface MemberRepository : JpaRepository<MemberJpaEntity, UUID>
