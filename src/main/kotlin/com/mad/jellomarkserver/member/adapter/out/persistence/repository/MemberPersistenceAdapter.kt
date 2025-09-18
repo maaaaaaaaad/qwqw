511fd20b5d23c72aa57da4be913702891b07b88d
maaaaaaaaad <integral5137@gmail.com>
2025-09-19 06:45:06 +0900

    Add method to check business registration number existence in `MemberPort` and its persistence adapter

--- src/main/kotlin/com/mad/jellomarkserver/member/adapter/out/persistence/repository/MemberPersistenceAdapter.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/adapter/out/persistence/repository/MemberPersistenceAdapter.kt
@@ -1,6 +1,7 @@
 package com.mad.jellomarkserver.member.adapter.out.persistence.repository
 
 import com.mad.jellomarkserver.member.adapter.out.persistence.mapper.MemberMapper
+import com.mad.jellomarkserver.member.core.domain.model.BusinessRegistrationNumber
 import com.mad.jellomarkserver.member.core.domain.model.Email
 import com.mad.jellomarkserver.member.core.domain.model.Member
 import com.mad.jellomarkserver.member.port.driven.MemberPort
@@ -13,6 +14,10 @@ class MemberPersistenceAdapter(
 ) : MemberPort {
     override fun existsByEmail(email: Email): Boolean {
         return jpaRepository.existsByEmail(email.value)
+    }
+
+    override fun existsByBusinessRegistrationNumber(businessRegistrationNumber: BusinessRegistrationNumber): Boolean {
+        return jpaRepository.existsByBusinessRegistrationNumber(businessRegistrationNumber.value)
     }
 
     override fun save(member: Member): Member {
--- src/main/kotlin/com/mad/jellomarkserver/member/port/driven/MemberPort.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/port/driven/MemberPort.kt
@@ -1,9 +1,11 @@
 package com.mad.jellomarkserver.member.port.driven
 
+import com.mad.jellomarkserver.member.core.domain.model.BusinessRegistrationNumber
 import com.mad.jellomarkserver.member.core.domain.model.Email
 import com.mad.jellomarkserver.member.core.domain.model.Member
 
 interface MemberPort {
     fun existsByEmail(email: Email): Boolean
+    fun existsByBusinessRegistrationNumber(businessRegistrationNumber: BusinessRegistrationNumber): Boolean
     fun save(member: Member): Member
 }
