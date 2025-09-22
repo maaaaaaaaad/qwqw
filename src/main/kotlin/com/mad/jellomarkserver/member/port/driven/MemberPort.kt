55347b127aac5dbb734be86743954afc876e557f
maaaaaaaaad <integral5137@gmail.com>
2025-09-22 17:51:16 +0900

    Remove unused existence check methods from `MemberPort` interface

--- src/main/kotlin/com/mad/jellomarkserver/member/port/driven/MemberPort.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/port/driven/MemberPort.kt
@@ -1,13 +1,7 @@
 package com.mad.jellomarkserver.member.port.driven
 
-import com.mad.jellomarkserver.member.core.domain.model.BusinessRegistrationNumber
-import com.mad.jellomarkserver.member.core.domain.model.Email
 import com.mad.jellomarkserver.member.core.domain.model.Member
-import com.mad.jellomarkserver.member.core.domain.model.Nickname
 
 interface MemberPort {
-    fun existsByEmail(email: Email): Boolean
-    fun existsByBusinessRegistrationNumber(businessRegistrationNumber: BusinessRegistrationNumber): Boolean
-    fun existsByNickname(nickname: Nickname): Boolean
     fun save(member: Member): Member
 }
