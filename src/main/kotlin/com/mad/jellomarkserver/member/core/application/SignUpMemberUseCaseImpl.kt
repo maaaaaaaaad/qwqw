4a9073c506bf3a5a6c7417d6548c9a6cb3a4677d
maaaaaaaaad <integral5137@gmail.com>
2025-09-22 17:51:34 +0900

    Remove unused duplicate check exceptions and validations from `SignUpMemberUseCaseImpl`

--- src/main/kotlin/com/mad/jellomarkserver/member/core/application/SignUpMemberUseCaseImpl.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/core/application/SignUpMemberUseCaseImpl.kt
@@ -1,9 +1,6 @@
 package com.mad.jellomarkserver.member.core.application
 
 import com.mad.jellomarkserver.member.core.domain.exception.BusinessNumberException
-import com.mad.jellomarkserver.member.core.domain.exception.DuplicateBrnException
-import com.mad.jellomarkserver.member.core.domain.exception.DuplicateEmailException
-import com.mad.jellomarkserver.member.core.domain.exception.DuplicateNicknameException
 import com.mad.jellomarkserver.member.core.domain.model.BusinessRegistrationNumber
 import com.mad.jellomarkserver.member.core.domain.model.Email
 import com.mad.jellomarkserver.member.core.domain.model.Member
@@ -13,25 +10,23 @@ import com.mad.jellomarkserver.member.port.driven.MemberPort
 import com.mad.jellomarkserver.member.port.driving.SignUpMemberCommand
 import com.mad.jellomarkserver.member.port.driving.SignUpMemberUseCase
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SignUpMemberUseCaseImpl(
     private val memberPort: MemberPort
 ) : SignUpMemberUseCase {
+
+    @Transactional
     override fun signUp(command: SignUpMemberCommand): Member {
         val email = Email.of(command.email)
-        if (memberPort.existsByEmail(email)) throw DuplicateEmailException(email.value)
         val nickname = Nickname.of(command.nickname)
-        if (memberPort.existsByNickname(nickname)) throw DuplicateNicknameException(nickname.value)
         val memberType = command.memberType
         val brn = when (memberType) {
             MemberType.OWNER -> command.businessRegistrationNumber?.let { BusinessRegistrationNumber.of(it) }
                 ?: throw BusinessNumberException()
 
             MemberType.CONSUMER -> null
-        }
-        if (brn != null && memberPort.existsByBusinessRegistrationNumber(brn)) {
-            throw DuplicateBrnException(brn.value)
         }
         val member = Member.create(nickname, email, memberType, brn)
         return memberPort.save(member)
