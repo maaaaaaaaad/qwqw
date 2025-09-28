962a2b08cccf1ceabfa0e8cfcc5411c54211dd28
maaaaaaaaad <integral5137@gmail.com>
2025-09-28 10:53:33 +0900

    delete unnecessary fields

--- src/main/kotlin/com/mad/jellomarkserver/member/port/driving/SignUpMemberCommand.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/port/driving/SignUpMemberCommand.kt
@@ -3,5 +3,4 @@ package com.mad.jellomarkserver.member.port.driving
 data class SignUpMemberCommand(
     val nickname: String,
     val email: String,
-    val businessRegistrationNumber: String? = null
 )
