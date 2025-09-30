98da84f533712a0bf5a0b01a89dff219325a4f4c
maaaaaaaaad <integral5137@gmail.com>
2025-09-30 16:42:09 +0900

    delete brn exception in the domain member

--- src/main/kotlin/com/mad/jellomarkserver/member/core/domain/exception/BusinessNumberException.kt
+++
@@ -1,6 +0,0 @@
-package com.mad.jellomarkserver.member.core.domain.exception
-
-class BusinessNumberException :
-    RuntimeException(
-        "Business number is not valid"
-    )
--- src/main/kotlin/com/mad/jellomarkserver/member/core/domain/exception/DuplicateBrnException.kt
+++
@@ -1,7 +0,0 @@
-package com.mad.jellomarkserver.member.core.domain.exception
-
-class DuplicateBrnException(brn: String) :
-    RuntimeException(
-        "Business registration number already in use: $brn"
-    ) {
-}
