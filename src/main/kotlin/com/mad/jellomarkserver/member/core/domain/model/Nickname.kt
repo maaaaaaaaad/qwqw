982fb9eb0a1ebfbff184d743403b791ecd5c055f
maaaaaaaaad <integral5137@gmail.com>
2025-10-03 16:33:14 +0900

    Refactor Nickname value class to enforce non-blank input and improve validation handling

--- src/main/kotlin/com/mad/jellomarkserver/member/core/domain/model/Nickname.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/core/domain/model/Nickname.kt
@@ -4,11 +4,16 @@ package com.mad.jellomarkserver.member.core.domain.model
 value class Nickname private constructor(val value: String) {
     companion object {
         private val pattern = Regex("^\\S{2,8}$")
-        fun of(input: String?): Nickname {
-            require(!input.isNullOrBlank())
+        fun of(input: String): Nickname {
+            require(input.isNotBlank())
             val trimmed = input.trim()
-            require(pattern.matches(trimmed))
-            return Nickname(trimmed)
+            try {
+                require(pattern.matches(trimmed))
+                return Nickname(trimmed)
+
+            } catch (ex: Exception) {
+                throw ex //Nickname Invalid exception 필요함
+            }
         }
     }
 }
