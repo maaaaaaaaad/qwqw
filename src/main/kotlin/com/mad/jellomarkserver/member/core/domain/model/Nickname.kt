b80df20b5ea161cc9180357e12f2699057a095e2
maaaaaaaaad <integral5137@gmail.com>
2025-10-05 10:33:17 +0900

    Refactor `Nickname` value class to improve validation and streamline exception handling

--- src/main/kotlin/com/mad/jellomarkserver/member/core/domain/model/Nickname.kt
+++ src/main/kotlin/com/mad/jellomarkserver/member/core/domain/model/Nickname.kt
@@ -7,15 +7,13 @@ value class Nickname private constructor(val value: String) {
     companion object {
         private val pattern = Regex("^\\S{2,8}$")
         fun of(input: String): Nickname {
-            require(input.isNotBlank())
             val trimmed = input.trim()
             try {
+                require(trimmed.isNotBlank())
                 require(pattern.matches(trimmed))
                 return Nickname(trimmed)
-
-            } catch (ex: Exception) {
-                if (ex is IllegalArgumentException) InvalidNicknameException(input)
-                throw ex
+            } catch (ex: IllegalArgumentException) {
+                throw InvalidNicknameException(trimmed)
             }
         }
     }
