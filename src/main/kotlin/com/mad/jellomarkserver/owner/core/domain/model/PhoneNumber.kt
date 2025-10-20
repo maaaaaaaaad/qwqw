2c48251db8fcc276e5d2815c86b2367404802bb0
maaaaaaaaad <integral5137@gmail.com>
2025-10-20 17:35:29 +0900

    Refactor `PhoneNumber` to use `@JvmInline` value class and enhance validation with regex pattern

--- src/main/kotlin/com/mad/jellomarkserver/owner/core/domain/model/PhoneNumber.kt
+++ src/main/kotlin/com/mad/jellomarkserver/owner/core/domain/model/PhoneNumber.kt
@@ -2,17 +2,18 @@ package com.mad.jellomarkserver.owner.core.domain.model
 
 import com.mad.jellomarkserver.owner.core.domain.exception.InvalidPhoneNumberException
 
-class PhoneNumber private constructor(val value: String) {
+@JvmInline
+value class PhoneNumber private constructor(val value: String) {
     companion object {
+        private val pattern = Regex("^(010|011|016|017|018|019)-\\d{3,4}-\\d{4}$|^02-\\d{3,4}-\\d{4}$|^0(3[1-3]|4[1-4]|5[1-5]|6[1-4])-\\d{3,4}-\\d{4}$")
         fun of(input: String): PhoneNumber {
             val trimmed = input.trim()
             try {
-                require(input.isNotBlank())
-                require(trimmed.length == 8)
-                require(trimmed.all { it.isDigit() })
+                require(trimmed.isNotBlank())
+                require(pattern.matches(trimmed))
                 return PhoneNumber(trimmed)
             } catch (ex: IllegalArgumentException) {
-                throw InvalidPhoneNumberException(input)
+                throw InvalidPhoneNumberException(trimmed)
             }
         }
     }
