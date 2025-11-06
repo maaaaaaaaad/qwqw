a7bdf21fe8fd0523b623e84c16e0eac41d2efda3
maaaaaaaaad <integral5137@gmail.com>
2025-11-06 17:45:02 +0900

    Add exception handlers for `InvalidOwnerNicknameException` and generic server errors

--- src/main/kotlin/com/mad/jellomarkserver/owner/adapter/driving/web/handler/OwnerRestExceptionHandler.kt
+++ src/main/kotlin/com/mad/jellomarkserver/owner/adapter/driving/web/handler/OwnerRestExceptionHandler.kt
@@ -1,10 +1,13 @@
 package com.mad.jellomarkserver.owner.adapter.driving.web.handler
 
+import com.mad.jellomarkserver.member.adapter.driving.web.response.ErrorResponse
 import com.mad.jellomarkserver.owner.core.domain.exception.DuplicateOwnerPhoneNumberException
 import com.mad.jellomarkserver.owner.core.domain.exception.InvalidOwnerBusinessNumberException
+import com.mad.jellomarkserver.owner.core.domain.exception.InvalidOwnerNicknameException
 import com.mad.jellomarkserver.owner.core.domain.exception.InvalidOwnerPhoneNumberException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -26,5 +29,20 @@ class OwnerRestExceptionHandler {
     fun handleDuplicatePhoneNumber(ex: DuplicateOwnerPhoneNumberException): ProblemDetail {
         val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
         return problemDetail
+    }
+
+    @ExceptionHandler(InvalidOwnerNicknameException::class)
+    fun handleInvalidNickname(ex: InvalidOwnerNicknameException): ProblemDetail {
+        val problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return problemDetail
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGeneric(ex: Exception): ResponseEntity<ErrorResponse> {
+        val body = ErrorResponse(
+            code = "INTERNAL_SERVER_ERROR",
+            message = "Unexpected server error"
+        )
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body)
     }
 }
