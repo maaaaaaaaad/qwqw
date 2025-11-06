507d22f67ab8f70202a818e58c79989b0073edab
maaaaaaaaad <integral5137@gmail.com>
2025-11-06 17:45:06 +0900

    Add `OwnerNickname` value class with validation logic and exception handling

--- src/main/kotlin/com/mad/jellomarkserver/owner/core/domain/model/OwnerNickname.kt
+++ src/main/kotlin/com/mad/jellomarkserver/owner/core/domain/model/OwnerNickname.kt
@@ -1,4 +1,20 @@
 package com.mad.jellomarkserver.owner.core.domain.model
 
-class OwnerNickname {
+import com.mad.jellomarkserver.owner.core.domain.exception.InvalidOwnerNicknameException
+
+@JvmInline
+value class OwnerNickname private constructor(val value: String) {
+    companion object {
+        private val pattern = Regex("^\\S{2,8}$")
+        fun of(input: String): OwnerNickname {
+            val trimmed = input.trim()
+            try {
+                require(trimmed.isNotBlank())
+                require(pattern.matches(trimmed))
+                return OwnerNickname(trimmed)
+            } catch (ex: IllegalArgumentException) {
+                throw InvalidOwnerNicknameException(trimmed)
+            }
+        }
+    }
 }
