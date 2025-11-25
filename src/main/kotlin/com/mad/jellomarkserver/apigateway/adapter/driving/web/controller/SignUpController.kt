fee79d6b29c084cce867680bf2aaff2a079ab95c
maaaaaaaaad <integral5137@gmail.com>
2025-11-25 15:14:58 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/SignUpController.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/SignUpController.kt
@@ -3,6 +3,7 @@ package com.mad.jellomarkserver.apigateway.adapter.driving.web.controller
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.request.SignUpMemberRequest
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.request.SignUpOwnerRequest
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.SignUpResponse
+import com.mad.jellomarkserver.apigateway.port.driving.SignUpMemberOrchestrator
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SignUpController {
+class SignUpController(
+    private val signUpMemberOrchestrator: SignUpMemberOrchestrator
+) {
 
     @PostMapping("/api/sign-up/member")
     @ResponseStatus(HttpStatus.CREATED)
     fun signUpMember(@RequestBody request: SignUpMemberRequest): SignUpResponse {
-        TODO("Not yet implemented")
+        return signUpMemberOrchestrator.signUp(request)
     }
 
     @PostMapping("/api/sign-up/owner")
