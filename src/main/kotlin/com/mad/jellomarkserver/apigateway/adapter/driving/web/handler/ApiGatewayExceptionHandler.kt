d0d56c98af54f68fd42593461d05ffc6b7ab876b
maaaaaaaaad <integral5137@gmail.com>
2025-11-25 15:14:56 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/handler/ApiGatewayExceptionHandler.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/handler/ApiGatewayExceptionHandler.kt
@@ -1,13 +1,38 @@
 package com.mad.jellomarkserver.apigateway.adapter.driving.web.handler
 
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.ErrorResponse
+import com.mad.jellomarkserver.member.core.domain.exception.DuplicateMemberEmailException
+import com.mad.jellomarkserver.member.core.domain.exception.DuplicateMemberNicknameException
+import com.mad.jellomarkserver.member.core.domain.exception.InvalidMemberEmailException
+import com.mad.jellomarkserver.member.core.domain.exception.InvalidMemberNicknameException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice(basePackages = ["com.mad.jellomarkserver.apigateway"])
 class ApiGatewayExceptionHandler {
+
+    @ExceptionHandler(DuplicateMemberEmailException::class)
+    fun handleDuplicateMemberEmail(ex: DuplicateMemberEmailException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+    }
+
+    @ExceptionHandler(InvalidMemberEmailException::class)
+    fun handleInvalidMemberEmail(ex: InvalidMemberEmailException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+    }
+
+    @ExceptionHandler(DuplicateMemberNicknameException::class)
+    fun handleDuplicateMemberNickname(ex: DuplicateMemberNicknameException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+    }
+
+    @ExceptionHandler(InvalidMemberNicknameException::class)
+    fun handleInvalidMemberNickname(ex: InvalidMemberNicknameException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+    }
 
     @ExceptionHandler(Exception::class)
     fun handleGeneric(ex: Exception): ResponseEntity<ErrorResponse> {
