672d762859d567634079e9507ddf41909e27ea3b
maaaaaaaaad <integral5137@gmail.com>
2025-11-29 18:56:09 +0900

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.mindrot:jbcrypt:0.4")
 
     developmentOnly("com.h2database:h2")
     testRuntimeOnly("com.h2database:h2")
