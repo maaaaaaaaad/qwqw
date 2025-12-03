80b13302969ef827e4f80cb56349b863d3885b7a
maaaaaaaaad <integral5137@gmail.com>
2025-12-03 15:21:04 +0900

    .

---
+++ src/main/kotlin/com/mad/jellomarkserver/auth/adapter/driven/persistence/repository/AuthJpaRepository.kt
@@ -0,0 +1,7 @@
+package com.mad.jellomarkserver.auth.adapter.driven.persistence.repository
+
+import com.mad.jellomarkserver.auth.adapter.driven.persistence.entity.AuthJpaEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface AuthJpaRepository : JpaRepository<AuthJpaEntity, UUID> {}
