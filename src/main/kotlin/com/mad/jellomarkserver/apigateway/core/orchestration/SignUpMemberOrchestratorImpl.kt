ed0e0178a4e62607be13caf89411e5577071f658
maaaaaaaaad <integral5137@gmail.com>
2025-12-04 16:44:29 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/core/orchestration/SignUpMemberOrchestratorImpl.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/core/orchestration/SignUpMemberOrchestratorImpl.kt
@@ -3,6 +3,8 @@ package com.mad.jellomarkserver.apigateway.core.orchestration
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.request.SignUpMemberRequest
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.SignUpResponse
 import com.mad.jellomarkserver.apigateway.port.driving.SignUpMemberOrchestrator
+import com.mad.jellomarkserver.auth.port.driving.SignUpAuthCommand
+import com.mad.jellomarkserver.auth.port.driving.SignUpAuthUseCase
 import com.mad.jellomarkserver.member.port.driving.SignUpMemberCommand
 import com.mad.jellomarkserver.member.port.driving.SignUpMemberUseCase
 import org.springframework.stereotype.Service
@@ -10,17 +12,26 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SignUpMemberOrchestratorImpl(
-    private val signUpMemberUseCase: SignUpMemberUseCase
+    private val signUpMemberUseCase: SignUpMemberUseCase,
+    private val signUpAuthUseCase: SignUpAuthUseCase
 ) : SignUpMemberOrchestrator {
 
     @Transactional
     override fun signUp(request: SignUpMemberRequest): SignUpResponse {
-        val command = SignUpMemberCommand(
+        val memberCommand = SignUpMemberCommand(
             nickname = request.nickname,
             email = request.email
         )
 
-        val member = signUpMemberUseCase.signUp(command)
+        val member = signUpMemberUseCase.signUp(memberCommand)
+
+        val authCommand = SignUpAuthCommand(
+            email = request.email,
+            password = request.password,
+            userType = "MEMBER"
+        )
+
+        signUpAuthUseCase.signUp(authCommand)
 
         return SignUpResponse.fromMember(member)
     }
