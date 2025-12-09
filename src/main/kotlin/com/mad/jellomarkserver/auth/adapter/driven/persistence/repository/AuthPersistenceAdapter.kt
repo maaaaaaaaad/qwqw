3221e64160252913fc36872a1259cb9b1243eda2
maaaaaaaaad <integral5137@gmail.com>
2025-12-09 11:19:22 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/auth/adapter/driven/persistence/repository/AuthPersistenceAdapter.kt
+++ src/main/kotlin/com/mad/jellomarkserver/auth/adapter/driven/persistence/repository/AuthPersistenceAdapter.kt
@@ -3,6 +3,7 @@ package com.mad.jellomarkserver.auth.adapter.driven.persistence.repository
 import com.mad.jellomarkserver.auth.adapter.driven.persistence.mapper.AuthMapper
 import com.mad.jellomarkserver.auth.core.domain.exception.DuplicateAuthEmailException
 import com.mad.jellomarkserver.auth.core.domain.model.Auth
+import com.mad.jellomarkserver.auth.core.domain.model.AuthEmail
 import com.mad.jellomarkserver.auth.port.driven.AuthPort
 import com.mad.jellomarkserver.common.persistence.ConstraintViolationTranslator
 import org.springframework.dao.DataIntegrityViolationException
@@ -26,5 +27,10 @@ class AuthPersistenceAdapter(
                 )
             )
         }
+    }
+
+    override fun findByEmail(email: AuthEmail): Auth? {
+        val entity = jpaRepository.findByEmail(email.value)
+        return entity?.let { mapper.toDomain(it) }
     }
 }
