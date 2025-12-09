e5fa68fd9206a4396e2228723ae5cda7db57b04b
maaaaaaaaad <integral5137@gmail.com>
2025-12-09 11:19:10 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/handler/ApiGatewayExceptionHandler.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/handler/ApiGatewayExceptionHandler.kt
@@ -1,6 +1,10 @@
 package com.mad.jellomarkserver.apigateway.adapter.driving.web.handler
 
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.ErrorResponse
+import com.mad.jellomarkserver.auth.core.domain.exception.AuthenticationFailedException
+import com.mad.jellomarkserver.auth.core.domain.exception.DuplicateAuthEmailException
+import com.mad.jellomarkserver.auth.core.domain.exception.InvalidAuthEmailException
+import com.mad.jellomarkserver.auth.core.domain.exception.InvalidRawPasswordException
 import com.mad.jellomarkserver.member.core.domain.exception.DuplicateMemberEmailException
 import com.mad.jellomarkserver.member.core.domain.exception.DuplicateMemberNicknameException
 import com.mad.jellomarkserver.member.core.domain.exception.InvalidMemberEmailException
@@ -13,6 +17,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice(basePackages = ["com.mad.jellomarkserver.apigateway"])
 class ApiGatewayExceptionHandler {
+
+    @ExceptionHandler(AuthenticationFailedException::class)
+    fun handleAuthenticationFailed(ex: AuthenticationFailedException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.message)
+    }
+
+    @ExceptionHandler(InvalidAuthEmailException::class)
+    fun handleInvalidAuthEmail(ex: InvalidAuthEmailException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+    }
+
+    @ExceptionHandler(DuplicateAuthEmailException::class)
+    fun handleDuplicateAuthEmail(ex: DuplicateAuthEmailException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+    }
+
+    @ExceptionHandler(InvalidRawPasswordException::class)
+    fun handleInvalidPassword(ex: InvalidRawPasswordException): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+    }
 
     @ExceptionHandler(DuplicateMemberEmailException::class)
     fun handleDuplicateMemberEmail(ex: DuplicateMemberEmailException): ProblemDetail {
