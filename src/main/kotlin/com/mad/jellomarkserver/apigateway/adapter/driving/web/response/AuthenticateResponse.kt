0101a2c18e99a0cd1c3a60f98cce4cf4f2f9eb37
maaaaaaaaad <integral5137@gmail.com>
2025-12-15 19:50:37 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/response/AuthenticateResponse.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/response/AuthenticateResponse.kt
@@ -3,5 +3,7 @@ package com.mad.jellomarkserver.apigateway.adapter.driving.web.response
 data class AuthenticateResponse(
     val authenticated: Boolean,
     val email: String?,
-    val userType: String?
+    val userType: String?,
+    val accessToken: String?,
+    val refreshToken: String?
 )
