b625a2b400650d4fbb3a7c8b0a750b690c035817
maaaaaaaaad <integral5137@gmail.com>
2025-12-15 19:50:32 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/AuthController.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/AuthController.kt
@@ -4,6 +4,8 @@ import com.mad.jellomarkserver.apigateway.adapter.driving.web.request.Authentica
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.AuthenticateResponse
 import com.mad.jellomarkserver.auth.port.driving.AuthenticateCommand
 import com.mad.jellomarkserver.auth.port.driving.AuthenticateUseCase
+import com.mad.jellomarkserver.auth.port.driving.IssueTokenCommand
+import com.mad.jellomarkserver.auth.port.driving.IssueTokenUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AuthController(
-    private val authenticateUseCase: AuthenticateUseCase
+    private val authenticateUseCase: AuthenticateUseCase,
+    private val issueTokenUseCase: IssueTokenUseCase
 ) {
 
     @PostMapping("/api/auth/authenticate")
@@ -23,10 +26,20 @@ class AuthController(
             password = request.password
         )
         val auth = authenticateUseCase.authenticate(command)
+
+        val tokenPair = issueTokenUseCase.execute(
+            IssueTokenCommand(
+                email = auth.email.value,
+                userType = auth.userType.name
+            )
+        )
+
         return AuthenticateResponse(
             authenticated = true,
             email = auth.email.value,
-            userType = auth.userType.name
+            userType = auth.userType.name,
+            accessToken = tokenPair.accessToken,
+            refreshToken = tokenPair.refreshToken
         )
     }
 }
