c47db3549a325d7785e8a86f8ea037483276862f
maaaaaaaaad <integral5137@gmail.com>
2025-12-19 19:41:49 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/AuthController.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/AuthController.kt
@@ -1,11 +1,10 @@
 package com.mad.jellomarkserver.apigateway.adapter.driving.web.controller
 
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.request.AuthenticateRequest
+import com.mad.jellomarkserver.apigateway.adapter.driving.web.request.RefreshTokenRequest
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.AuthenticateResponse
-import com.mad.jellomarkserver.auth.port.driving.AuthenticateCommand
-import com.mad.jellomarkserver.auth.port.driving.AuthenticateUseCase
-import com.mad.jellomarkserver.auth.port.driving.IssueTokenCommand
-import com.mad.jellomarkserver.auth.port.driving.IssueTokenUseCase
+import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.RefreshTokenResponse
+import com.mad.jellomarkserver.auth.port.driving.*
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AuthController(
     private val authenticateUseCase: AuthenticateUseCase,
-    private val issueTokenUseCase: IssueTokenUseCase
+    private val issueTokenUseCase: IssueTokenUseCase,
+    private val refreshTokenUseCase: RefreshTokenUseCase
 ) {
 
     @PostMapping("/api/auth/authenticate")
@@ -38,6 +38,20 @@ class AuthController(
             authenticated = true,
             email = auth.email.value,
             userType = auth.userType.name,
+            accessToken = tokenPair.accessToken,
+            refreshToken = tokenPair.refreshToken
+        )
+    }
+
+    @PostMapping("/api/auth/refresh")
+    @ResponseStatus(HttpStatus.OK)
+    fun refresh(@RequestBody request: RefreshTokenRequest): RefreshTokenResponse {
+        val command = RefreshTokenCommand(
+            refreshToken = request.refreshToken
+        )
+        val tokenPair = refreshTokenUseCase.execute(command)
+
+        return RefreshTokenResponse(
             accessToken = tokenPair.accessToken,
             refreshToken = tokenPair.refreshToken
         )
