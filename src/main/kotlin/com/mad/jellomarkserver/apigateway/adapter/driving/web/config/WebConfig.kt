6185303d7e347a4c3bc5aba0fb3a9ba83b36cd1d
maaaaaaaaad <integral5137@gmail.com>
2025-12-20 13:57:00 +0900

    .

---
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/config/WebConfig.kt
@@ -0,0 +1,22 @@
+package com.mad.jellomarkserver.apigateway.adapter.driving.web.config
+
+import com.mad.jellomarkserver.apigateway.adapter.driving.web.interceptor.JwtAuthenticationInterceptor
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    private val jwtAuthenticationInterceptor: JwtAuthenticationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(jwtAuthenticationInterceptor)
+            .addPathPatterns("/api/**")
+            .excludePathPatterns(
+                "/api/sign-up/**",
+                "/api/owners/sign-up",
+                "/api/auth/authenticate",
+                "/api/auth/refresh"
+            )
+    }
+}
