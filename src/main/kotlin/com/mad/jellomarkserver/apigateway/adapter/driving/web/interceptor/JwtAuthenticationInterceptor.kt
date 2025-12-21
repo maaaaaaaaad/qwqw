5e950e3c2113521ec134d9bd3b4b3b11a2966f5e
maaaaaaaaad <integral5137@gmail.com>
2025-12-21 10:28:31 +0900

    .

---
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/interceptor/JwtAuthenticationInterceptor.kt
@@ -0,0 +1,41 @@
+package com.mad.jellomarkserver.apigateway.adapter.driving.web.interceptor
+
+import com.mad.jellomarkserver.auth.adapter.driven.jwt.JwtTokenProvider
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+
+@Component
+class JwtAuthenticationInterceptor(
+    private val jwtTokenProvider: JwtTokenProvider
+) : HandlerInterceptor {
+    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+        val authHeader = request.getHeader("Authorization")
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.status = HttpServletResponse.SC_UNAUTHORIZED
+            return false
+        }
+
+        val token = authHeader.substring(7)
+
+        if (!jwtTokenProvider.validateToken(token)) {
+            response.status = HttpServletResponse.SC_UNAUTHORIZED
+            return false
+        }
+
+        try {
+            val email = jwtTokenProvider.getEmailFromToken(token)
+            val userType = jwtTokenProvider.getUserTypeFromToken(token)
+
+            request.setAttribute("email", email)
+            request.setAttribute("userType", userType)
+
+            return true
+        } catch (e: Exception) {
+            response.status = HttpServletResponse.SC_UNAUTHORIZED
+            return false
+        }
+    }
+}
