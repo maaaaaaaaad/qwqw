56d838e4676bb8dd8515654213084e21d839350a
maaaaaaaaad <integral5137@gmail.com>
2025-12-21 13:34:39 +0900

    .

--- src/main/kotlin/com/mad/jellomarkserver/owner/core/domain/model/Owner.kt
+++ src/main/kotlin/com/mad/jellomarkserver/owner/core/domain/model/Owner.kt
@@ -8,17 +8,32 @@ class Owner private constructor(
     val businessNumber: BusinessNumber,
     val ownerPhoneNumber: OwnerPhoneNumber,
     val ownerNickname: OwnerNickname,
+    val ownerEmail: OwnerEmail,
     val createdAt: Instant,
     val updatedAt: Instant,
 ) {
     companion object {
-        fun create(businessNumber: BusinessNumber, ownerPhoneNumber: OwnerPhoneNumber, ownerNickname: OwnerNickname, clock: Clock = Clock.systemUTC()): Owner {
+        fun create(
+            businessNumber: BusinessNumber,
+            ownerPhoneNumber: OwnerPhoneNumber,
+            ownerNickname: OwnerNickname,
+            ownerEmail: OwnerEmail,
+            clock: Clock = Clock.systemUTC()
+        ): Owner {
             val now = Instant.now(clock)
-            return Owner(OwnerId.new(), businessNumber, ownerPhoneNumber, ownerNickname, now, now)
+            return Owner(OwnerId.new(), businessNumber, ownerPhoneNumber, ownerNickname, ownerEmail, now, now)
         }
 
-        fun reconstruct(id: OwnerId, businessNumber: BusinessNumber, ownerPhoneNumber: OwnerPhoneNumber, ownerNickname: OwnerNickname, createdAt: Instant, updatedAt: Instant): Owner {
-            return Owner(id, businessNumber, ownerPhoneNumber, ownerNickname, createdAt, updatedAt)
+        fun reconstruct(
+            id: OwnerId,
+            businessNumber: BusinessNumber,
+            ownerPhoneNumber: OwnerPhoneNumber,
+            ownerNickname: OwnerNickname,
+            ownerEmail: OwnerEmail,
+            createdAt: Instant,
+            updatedAt: Instant
+        ): Owner {
+            return Owner(id, businessNumber, ownerPhoneNumber, ownerNickname, ownerEmail, createdAt, updatedAt)
         }
     }
 
