898f3e70b55163816ed8539c0225590879b9e060
maaaaaaaaad <integral5137@gmail.com>
2026-01-01 13:42:53 +0900

    .

---
+++ src/main/kotlin/com/mad/jellomarkserver/beautishop/port/driving/CreateBeautishopCommand.kt
@@ -0,0 +1,14 @@
+package com.mad.jellomarkserver.beautishop.port.driving
+
+data class CreateBeautishopCommand(
+    val ownerId: String,
+    val shopName: String,
+    val shopRegNum: String,
+    val shopPhoneNumber: String,
+    val shopAddress: String,
+    val latitude: Double,
+    val longitude: Double,
+    val operatingTime: Map<String, String>,
+    val shopDescription: String?,
+    val shopImage: String?
+)
