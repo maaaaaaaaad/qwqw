af9868527b0fce92552bd567da6849a0a6170957
maaaaaaaaad <integral5137@gmail.com>
2026-01-02 16:59:26 +0900

    .

---
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/CreateBeautishopRequest.kt
@@ -0,0 +1,13 @@
+package com.mad.jellomarkserver.apigateway.adapter.driving.web.request
+
+data class CreateBeautishopRequest(
+    val shopName: String,
+    val shopRegNum: String,
+    val shopPhoneNumber: String,
+    val shopAddress: String,
+    val latitude: Double,
+    val longitude: Double,
+    val operatingTime: Map<String, String>,
+    val shopDescription: String?,
+    val shopImage: String?
+)
