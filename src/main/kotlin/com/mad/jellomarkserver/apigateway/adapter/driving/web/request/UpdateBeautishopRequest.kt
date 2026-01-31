463247253ca8a3cfc303f0debcf3a26a43c1b153
maaaaaaaaad <integral5137@gmail.com>
2026-01-31 11:12:16 +0900

    refactor: UpdateBeautishopRequest for multi-image

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/UpdateBeautishopRequest.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/UpdateBeautishopRequest.kt
@@ -3,5 +3,5 @@ package com.mad.jellomarkserver.apigateway.adapter.driving.web.request
 data class UpdateBeautishopRequest(
     val operatingTime: Map<String, String>?,
     val shopDescription: String?,
-    val shopImage: String?
+    val shopImages: List<String>?
 )
