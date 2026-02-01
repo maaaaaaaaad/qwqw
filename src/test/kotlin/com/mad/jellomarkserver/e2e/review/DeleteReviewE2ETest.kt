46749af457892091396c17bcfc46af0e93c1bb8c
maaaaaaaaad <integral5137@gmail.com>
2026-02-01 14:23:04 +0900

    refactor: DeleteReviewE2ETest for multi-image

--- src/test/kotlin/com/mad/jellomarkserver/e2e/review/DeleteReviewE2ETest.kt
+++ src/test/kotlin/com/mad/jellomarkserver/e2e/review/DeleteReviewE2ETest.kt
@@ -84,7 +84,7 @@ class DeleteReviewE2ETest {
             longitude = 127.0276,
             operatingTime = mapOf("Monday" to "09:00-18:00"),
             shopDescription = "Test beauty salon",
-            shopImage = null
+            shopImages = null
         )
 
         val headers = HttpHeaders().apply {
