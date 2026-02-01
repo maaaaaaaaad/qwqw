26483db0b04bf7722a5f9a7e56da0f80d093d9bd
maaaaaaaaad <integral5137@gmail.com>
2026-02-01 14:23:03 +0900

    refactor: CreateReviewE2ETest for multi-image

--- src/test/kotlin/com/mad/jellomarkserver/e2e/review/CreateReviewE2ETest.kt
+++ src/test/kotlin/com/mad/jellomarkserver/e2e/review/CreateReviewE2ETest.kt
@@ -83,7 +83,7 @@ class CreateReviewE2ETest {
             longitude = 127.0276,
             operatingTime = mapOf("Monday" to "09:00-18:00"),
             shopDescription = "Test beauty salon",
-            shopImage = null
+            shopImages = null
         )
 
         val headers = HttpHeaders().apply {
