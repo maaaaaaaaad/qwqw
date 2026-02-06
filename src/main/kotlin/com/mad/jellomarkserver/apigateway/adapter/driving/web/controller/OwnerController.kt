0121ebbd99d9e98d3d9e0fa92b9515477e2f4f31
maaaaaaaaad <integral5137@gmail.com>
2026-02-06 16:31:16 +0900

    add: GET /api/owners/me/beautishops endpoint

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/OwnerController.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/controller/OwnerController.kt
@@ -1,6 +1,11 @@
 package com.mad.jellomarkserver.apigateway.adapter.driving.web.controller
 
+import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.BeautishopResponse
 import com.mad.jellomarkserver.apigateway.adapter.driving.web.response.OwnerResponse
+import com.mad.jellomarkserver.beautishop.core.domain.model.ShopId
+import com.mad.jellomarkserver.beautishop.port.driving.GetOwnerBeautishopsCommand
+import com.mad.jellomarkserver.beautishop.port.driving.GetOwnerBeautishopsUseCase
+import com.mad.jellomarkserver.category.port.driven.ShopCategoryPort
 import com.mad.jellomarkserver.owner.port.driving.GetCurrentOwnerCommand
 import com.mad.jellomarkserver.owner.port.driving.GetCurrentOwnerUseCase
 import jakarta.servlet.http.HttpServletRequest
@@ -11,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class OwnerController(
-    private val getCurrentOwnerUseCase: GetCurrentOwnerUseCase
+    private val getCurrentOwnerUseCase: GetCurrentOwnerUseCase,
+    private val getOwnerBeautishopsUseCase: GetOwnerBeautishopsUseCase,
+    private val shopCategoryPort: ShopCategoryPort
 ) {
     @GetMapping("/api/owners/me")
     @ResponseStatus(HttpStatus.OK)
@@ -20,5 +27,17 @@ class OwnerController(
         val command = GetCurrentOwnerCommand(email = email)
         val owner = getCurrentOwnerUseCase.execute(command)
         return OwnerResponse.from(owner)
+    }
+
+    @GetMapping("/api/owners/me/beautishops")
+    @ResponseStatus(HttpStatus.OK)
+    fun getOwnerBeautishops(request: HttpServletRequest): List<BeautishopResponse> {
+        val email = request.getAttribute("email") as String
+        val command = GetOwnerBeautishopsCommand(email = email)
+        val beautishops = getOwnerBeautishopsUseCase.execute(command)
+        return beautishops.map { shop ->
+            val categories = shopCategoryPort.findCategoriesByShopId(shop.id)
+            BeautishopResponse.from(shop, categories)
+        }
     }
 }
