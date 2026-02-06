f5d7b99a4ce07cc41ac8fdf83287b0efbaa3b384
maaaaaaaaad <integral5137@gmail.com>
2026-02-06 16:31:25 +0900

    fix: migrate shopImage to shopImages in ListBeautishopsE2ETest

--- src/test/kotlin/com/mad/jellomarkserver/e2e/beautishop/ListBeautishopsE2ETest.kt
+++ src/test/kotlin/com/mad/jellomarkserver/e2e/beautishop/ListBeautishopsE2ETest.kt
@@ -67,7 +67,7 @@ class ListBeautishopsE2ETest {
             longitude = 127.0276,
             operatingTime = mapOf("Monday" to "09:00-18:00"),
             shopDescription = "Premium beauty salon",
-            shopImage = "https://example.com/image.jpg"
+            shopImages = listOf("https://example.com/image.jpg")
         )
 
         val headers = HttpHeaders().apply {
