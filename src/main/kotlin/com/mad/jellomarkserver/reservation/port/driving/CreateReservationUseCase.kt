d48ed23c65f9bdbcfaa476e0fe76c6b117517b5f
maaaaaaaaad <integral5137@gmail.com>
2026-02-08 01:47:51 +0900

    add: CreateReservationUseCase interface

---
+++ src/main/kotlin/com/mad/jellomarkserver/reservation/port/driving/CreateReservationUseCase.kt
@@ -0,0 +1,7 @@
+package com.mad.jellomarkserver.reservation.port.driving
+
+import com.mad.jellomarkserver.reservation.core.domain.model.Reservation
+
+fun interface CreateReservationUseCase {
+    fun execute(command: CreateReservationCommand): Reservation
+}
