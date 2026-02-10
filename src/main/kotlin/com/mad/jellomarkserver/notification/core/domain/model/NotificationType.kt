cfb1759745c5661eb80432c618a76c49231b1b01
maaaaaaaaad <integral5137@gmail.com>
2026-02-10 14:29:56 +0900

    add: NotificationType enum for reservation events

---
+++ src/main/kotlin/com/mad/jellomarkserver/notification/core/domain/model/NotificationType.kt
@@ -0,0 +1,10 @@
+package com.mad.jellomarkserver.notification.core.domain.model
+
+enum class NotificationType {
+    RESERVATION_CREATED,
+    RESERVATION_CONFIRMED,
+    RESERVATION_REJECTED,
+    RESERVATION_CANCELLED,
+    RESERVATION_COMPLETED,
+    RESERVATION_NO_SHOW
+}
