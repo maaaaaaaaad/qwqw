123f9c6eeca31367c9021b1977ebbe4b3cd73a29
maaaaaaaaad <integral5137@gmail.com>
2026-02-26 11:47:02 +0900

    fix: update CreateReviewE2ETest for 10-digit business number

--- src/test/kotlin/com/mad/jellomarkserver/e2e/review/CreateReviewE2ETest.kt
+++ src/test/kotlin/com/mad/jellomarkserver/e2e/review/CreateReviewE2ETest.kt
@@ -53,7 +53,7 @@ class CreateReviewE2ETest {
 
     private fun signUpOwnerAndGetAccessToken(): String {
         val request = SignUpOwnerRequest(
-            businessNumber = "123456789",
+            businessNumber = "1234567890",
             phoneNumber = "010-1234-5678",
             nickname = "testshop",
             email = "owner@example.com",
