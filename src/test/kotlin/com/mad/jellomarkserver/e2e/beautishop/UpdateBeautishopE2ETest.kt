ebedfbcf2522606dc95e0186bd9a9407e1ab4963
maaaaaaaaad <integral5137@gmail.com>
2026-02-26 11:46:55 +0900

    fix: update UpdateBeautishopE2ETest for 10-digit business number

--- src/test/kotlin/com/mad/jellomarkserver/e2e/beautishop/UpdateBeautishopE2ETest.kt
+++ src/test/kotlin/com/mad/jellomarkserver/e2e/beautishop/UpdateBeautishopE2ETest.kt
@@ -42,7 +42,7 @@ class UpdateBeautishopE2ETest {
 
     private fun signUpOwnerAndGetAccessToken(
         email: String = "owner@example.com",
-        businessNumber: String = "123456789",
+        businessNumber: String = "1234567890",
         phoneNumber: String = "010-1234-5678",
         nickname: String = "testshop"
     ): String {
@@ -171,7 +171,7 @@ class UpdateBeautishopE2ETest {
     fun `should return 403 when owner tries to update another owner's beautishop`() {
         val ownerToken1 = signUpOwnerAndGetAccessToken(
             email = "owner1@example.com",
-            businessNumber = "123456789",
+            businessNumber = "1234567890",
             phoneNumber = "010-1111-1111",
             nickname = "owner1"
         )
@@ -179,7 +179,7 @@ class UpdateBeautishopE2ETest {
 
         val ownerToken2 = signUpOwnerAndGetAccessToken(
             email = "owner2@example.com",
-            businessNumber = "987654321",
+            businessNumber = "9876543210",
             phoneNumber = "010-2222-2222",
             nickname = "owner2"
         )
