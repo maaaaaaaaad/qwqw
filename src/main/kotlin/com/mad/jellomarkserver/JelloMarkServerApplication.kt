017149e68346da88ca2dd5deb904c99d0c884718
maaaaaaaaad <integral5137@gmail.com>
2026-03-01 21:19:20 +0900

    add: enable scheduling in Spring Boot application

--- src/main/kotlin/com/mad/jellomarkserver/JelloMarkServerApplication.kt
+++ src/main/kotlin/com/mad/jellomarkserver/JelloMarkServerApplication.kt
@@ -2,8 +2,10 @@ package com.mad.jellomarkserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class JelloMarkServerApplication
 
 fun main(args: Array<String>) {
