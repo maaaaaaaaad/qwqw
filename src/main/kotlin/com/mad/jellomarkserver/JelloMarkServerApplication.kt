e98b487a3ae6ead10472bc8c47b46604ecbfa2d3
maaaaaaaaad <integral5137@gmail.com>
2026-03-16 18:17:25 +0900

    add: @EnableAsync 설정 추가

--- src/main/kotlin/com/mad/jellomarkserver/JelloMarkServerApplication.kt
+++ src/main/kotlin/com/mad/jellomarkserver/JelloMarkServerApplication.kt
@@ -2,10 +2,12 @@ package com.mad.jellomarkserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 class JelloMarkServerApplication
 
 fun main(args: Array<String>) {
