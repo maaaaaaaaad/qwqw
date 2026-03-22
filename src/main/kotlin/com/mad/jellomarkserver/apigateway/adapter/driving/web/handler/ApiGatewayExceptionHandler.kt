1489b9e7a0d371a0852115644e7d62d7cd46b846
maaaaaaaaad <integral5137@gmail.com>
2026-03-22 18:49:50 +0900

    refactor: ProblemDetail 응답에 에러 코드 포함

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/handler/ApiGatewayExceptionHandler.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/handler/ApiGatewayExceptionHandler.kt
@@ -25,283 +25,298 @@ class ApiGatewayExceptionHandler {
 
     private val log = LoggerFactory.getLogger(ApiGatewayExceptionHandler::class.java)
 
+    private fun toErrorCode(ex: Exception): String {
+        return ex::class.simpleName
+            ?.removeSuffix("Exception")
+            ?.replace(Regex("([a-z])([A-Z])"), "$1_$2")
+            ?.replace(Regex("([A-Z]+)([A-Z][a-z])"), "$1_$2")
+            ?.uppercase()
+            ?: "UNKNOWN"
+    }
+
+    private fun withCode(status: HttpStatus, ex: Exception): ProblemDetail {
+        val pd = ProblemDetail.forStatusAndDetail(status, ex.message)
+        pd.setProperty("code", toErrorCode(ex))
+        return pd
+    }
+
     @ExceptionHandler(AuthenticationFailedException::class)
     fun handleAuthenticationFailed(ex: AuthenticationFailedException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.message)
+        return withCode(HttpStatus.UNAUTHORIZED, ex)
     }
 
     @ExceptionHandler(InvalidTokenException::class)
     fun handleInvalidToken(ex: InvalidTokenException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.message)
+        return withCode(HttpStatus.UNAUTHORIZED, ex)
     }
 
     @ExceptionHandler(InvalidKakaoTokenException::class)
     fun handleInvalidKakaoToken(ex: InvalidKakaoTokenException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.message)
+        return withCode(HttpStatus.UNAUTHORIZED, ex)
     }
 
     @ExceptionHandler(KakaoApiException::class)
     fun handleKakaoApiException(ex: KakaoApiException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_GATEWAY, ex.message)
+        return withCode(HttpStatus.BAD_GATEWAY, ex)
     }
 
     @ExceptionHandler(InvalidAuthEmailException::class)
     fun handleInvalidAuthEmail(ex: InvalidAuthEmailException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(DuplicateAuthEmailException::class)
     fun handleDuplicateAuthEmail(ex: DuplicateAuthEmailException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidRawPasswordException::class)
     fun handleInvalidPassword(ex: InvalidRawPasswordException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(DuplicateSocialAccountException::class)
     fun handleDuplicateSocialAccount(ex: DuplicateSocialAccountException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(DuplicateMemberNicknameException::class)
     fun handleDuplicateMemberNickname(ex: DuplicateMemberNicknameException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidMemberNicknameException::class)
     fun handleInvalidMemberNickname(ex: InvalidMemberNicknameException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(MemberNotFoundException::class)
     fun handleMemberNotFound(ex: MemberNotFoundException): ProblemDetail {
         log.warn("MemberNotFound: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+        return withCode(HttpStatus.NOT_FOUND, ex)
     }
 
     @ExceptionHandler(DuplicateOwnerNicknameException::class)
     fun handleDuplicateOwnerNickname(ex: DuplicateOwnerNicknameException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidOwnerNicknameException::class)
     fun handleInvalidOwnerNickname(ex: InvalidOwnerNicknameException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(DuplicateOwnerBusinessNumberException::class)
     fun handleDuplicateOwnerBusinessNumber(ex: DuplicateOwnerBusinessNumberException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidOwnerBusinessNumberException::class)
     fun handleInvalidOwnerBusinessNumber(ex: InvalidOwnerBusinessNumberException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(DuplicateOwnerPhoneNumberException::class)
     fun handleDuplicateOwnerPhoneNumber(ex: DuplicateOwnerPhoneNumberException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidOwnerPhoneNumberException::class)
     fun handleInvalidOwnerPhoneNumber(ex: InvalidOwnerPhoneNumberException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(DuplicateOwnerEmailException::class)
     fun handleDuplicateOwnerEmail(ex: DuplicateOwnerEmailException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidOwnerEmailException::class)
     fun handleInvalidOwnerEmail(ex: InvalidOwnerEmailException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidShopNameException::class)
     fun handleInvalidShopName(ex: InvalidShopNameException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidShopRegNumException::class)
     fun handleInvalidShopRegNum(ex: InvalidShopRegNumException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(DuplicateShopRegNumException::class)
     fun handleDuplicateShopRegNum(ex: DuplicateShopRegNumException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(InvalidShopPhoneNumberException::class)
     fun handleInvalidShopPhoneNumber(ex: InvalidShopPhoneNumberException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidShopAddressException::class)
     fun handleInvalidShopAddress(ex: InvalidShopAddressException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidShopGPSException::class)
     fun handleInvalidShopGPS(ex: InvalidShopGPSException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidOperatingTimeException::class)
     fun handleInvalidOperatingTime(ex: InvalidOperatingTimeException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidShopDescriptionException::class)
     fun handleInvalidShopDescription(ex: InvalidShopDescriptionException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidShopImageException::class)
     fun handleInvalidShopImage(ex: InvalidShopImageException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(BeautishopNotFoundException::class)
     fun handleBeautishopNotFound(ex: BeautishopNotFoundException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+        return withCode(HttpStatus.NOT_FOUND, ex)
     }
 
     @ExceptionHandler(InvalidReviewRatingException::class)
     fun handleInvalidReviewRating(ex: InvalidReviewRatingException): ProblemDetail {
         log.warn("InvalidReviewRating: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidReviewContentException::class)
     fun handleInvalidReviewContent(ex: InvalidReviewContentException): ProblemDetail {
         log.warn("InvalidReviewContent: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidReviewImagesException::class)
     fun handleInvalidReviewImages(ex: InvalidReviewImagesException): ProblemDetail {
         log.warn("InvalidReviewImages: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(ReviewNotFoundException::class)
     fun handleReviewNotFound(ex: ReviewNotFoundException): ProblemDetail {
         log.warn("ReviewNotFound: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+        return withCode(HttpStatus.NOT_FOUND, ex)
     }
 
     @ExceptionHandler(InvalidReplyContentException::class)
     fun handleInvalidReplyContent(ex: InvalidReplyContentException): ProblemDetail {
         log.warn("InvalidReplyContent: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(UnauthorizedReviewAccessException::class)
     fun handleUnauthorizedReviewAccess(ex: UnauthorizedReviewAccessException): ProblemDetail {
         log.warn("UnauthorizedReviewAccess: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.message)
+        return withCode(HttpStatus.FORBIDDEN, ex)
     }
 
     @ExceptionHandler(DuplicateReviewException::class)
     fun handleDuplicateReview(ex: DuplicateReviewException): ProblemDetail {
         log.warn("DuplicateReview: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(EmptyReviewException::class)
     fun handleEmptyReview(ex: EmptyReviewException): ProblemDetail {
         log.warn("EmptyReview: {}", ex.message)
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(CategoryNotFoundException::class)
     fun handleCategoryNotFound(ex: CategoryNotFoundException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+        return withCode(HttpStatus.NOT_FOUND, ex)
     }
 
     @ExceptionHandler(UnauthorizedShopAccessException::class)
     fun handleUnauthorizedShopAccess(ex: UnauthorizedShopAccessException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.message)
+        return withCode(HttpStatus.FORBIDDEN, ex)
     }
 
     @ExceptionHandler(UnauthorizedBeautishopAccessException::class)
     fun handleUnauthorizedBeautishopAccess(ex: UnauthorizedBeautishopAccessException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.message)
+        return withCode(HttpStatus.FORBIDDEN, ex)
     }
 
     @ExceptionHandler(InvalidTreatmentNameException::class)
     fun handleInvalidTreatmentName(ex: InvalidTreatmentNameException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidTreatmentPriceException::class)
     fun handleInvalidTreatmentPrice(ex: InvalidTreatmentPriceException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidTreatmentDurationException::class)
     fun handleInvalidTreatmentDuration(ex: InvalidTreatmentDurationException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidTreatmentDescriptionException::class)
     fun handleInvalidTreatmentDescription(ex: InvalidTreatmentDescriptionException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(TreatmentNotFoundException::class)
     fun handleTreatmentNotFound(ex: TreatmentNotFoundException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+        return withCode(HttpStatus.NOT_FOUND, ex)
     }
 
     @ExceptionHandler(UnauthorizedTreatmentAccessException::class)
     fun handleUnauthorizedTreatmentAccess(ex: UnauthorizedTreatmentAccessException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.message)
+        return withCode(HttpStatus.FORBIDDEN, ex)
     }
 
     @ExceptionHandler(ReservationNotFoundException::class)
     fun handleReservationNotFound(ex: ReservationNotFoundException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+        return withCode(HttpStatus.NOT_FOUND, ex)
     }
 
     @ExceptionHandler(InvalidReservationStatusTransitionException::class)
     fun handleInvalidReservationStatusTransition(ex: InvalidReservationStatusTransitionException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(ReservationTimeConflictException::class)
     fun handleReservationTimeConflict(ex: ReservationTimeConflictException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.message)
+        return withCode(HttpStatus.CONFLICT, ex)
     }
 
     @ExceptionHandler(PastReservationException::class)
     fun handlePastReservation(ex: PastReservationException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(TreatmentNotInShopException::class)
     fun handleTreatmentNotInShop(ex: TreatmentNotInShopException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(UnauthorizedReservationAccessException::class)
     fun handleUnauthorizedReservationAccess(ex: UnauthorizedReservationAccessException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.message)
+        return withCode(HttpStatus.FORBIDDEN, ex)
     }
 
     @ExceptionHandler(InvalidReservationMemoException::class)
     fun handleInvalidReservationMemo(ex: InvalidReservationMemoException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(InvalidRejectionReasonException::class)
     fun handleInvalidRejectionReason(ex: InvalidRejectionReasonException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.message)
+        return withCode(HttpStatus.UNPROCESSABLE_ENTITY, ex)
     }
 
     @ExceptionHandler(Exception::class)
