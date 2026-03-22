1783a56549fc54a62dc43c08d6a9c1667f117baa
maaaaaaaaad <integral5137@gmail.com>
2026-03-22 16:04:07 +0900

    fix: add reservationId to CreateReviewCommand

--- src/main/kotlin/com/mad/jellomarkserver/review/port/driving/CreateReviewCommand.kt
+++ src/main/kotlin/com/mad/jellomarkserver/review/port/driving/CreateReviewCommand.kt
@@ -3,6 +3,7 @@ package com.mad.jellomarkserver.review.port.driving
 data class CreateReviewCommand(
     val shopId: String,
     val memberId: String,
+    val reservationId: String?,
     val rating: Int?,
     val content: String?,
     val images: List<String>?
