43dd9ce7cc895b54f4c0aa2c93fbde0bd399f16d
maaaaaaaaad <integral5137@gmail.com>
2026-03-22 16:04:07 +0900

    fix: add reservationId to CreateReviewRequest

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/CreateReviewRequest.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/CreateReviewRequest.kt
@@ -1,6 +1,7 @@
 package com.mad.jellomarkserver.apigateway.adapter.driving.web.request
 
 data class CreateReviewRequest(
+    val reservationId: String?,
     val rating: Int?,
     val content: String?,
     val images: List<String>?
