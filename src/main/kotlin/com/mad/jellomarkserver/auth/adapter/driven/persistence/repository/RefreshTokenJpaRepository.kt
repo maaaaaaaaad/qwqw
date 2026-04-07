7b1745c8b3557240bf4da25ed0ee6f5091e18a06
maaaaaaaaad <integral5137@gmail.com>
2026-04-07 13:12:35 +0900

    fix: use modifying query for refresh token deletion

--- src/main/kotlin/com/mad/jellomarkserver/auth/adapter/driven/persistence/repository/RefreshTokenJpaRepository.kt
+++ src/main/kotlin/com/mad/jellomarkserver/auth/adapter/driven/persistence/repository/RefreshTokenJpaRepository.kt
@@ -2,10 +2,15 @@ package com.mad.jellomarkserver.auth.adapter.driven.persistence.repository
 
 import com.mad.jellomarkserver.auth.adapter.driven.persistence.entity.RefreshTokenJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface RefreshTokenJpaRepository : JpaRepository<RefreshTokenJpaEntity, UUID> {
     fun findByIdentifier(identifier: String): RefreshTokenJpaEntity?
     fun findByToken(token: String): RefreshTokenJpaEntity?
+
+    @Modifying
+    @Query("DELETE FROM RefreshTokenJpaEntity r WHERE r.identifier = :identifier")
     fun deleteByIdentifier(identifier: String)
 }
