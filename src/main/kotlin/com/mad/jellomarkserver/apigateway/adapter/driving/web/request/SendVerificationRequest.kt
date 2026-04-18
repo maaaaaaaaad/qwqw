f36d9f61bd0aa76880747c543a06ae54c20ac0dc
maaaaaaaaad <integral5137@gmail.com>
2026-04-18 12:41:35 +0900

    fix: make purpose nullable for backward compatibility with Jackson

--- src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/SendVerificationRequest.kt
+++ src/main/kotlin/com/mad/jellomarkserver/apigateway/adapter/driving/web/request/SendVerificationRequest.kt
@@ -3,5 +3,5 @@ package com.mad.jellomarkserver.apigateway.adapter.driving.web.request
 data class SendVerificationRequest(
     val target: String,
     val type: String,
-    val purpose: String = "SIGNUP"
+    val purpose: String? = null
 )
