8e8eeaa1721950e4d9682a3173e3339d2fbc3d4c
maaaaaaaaad <integral5137@gmail.com>
2026-04-19 19:06:12 +0900

    add: OwnerPort에 delete 메서드 추가

--- src/main/kotlin/com/mad/jellomarkserver/owner/port/driven/OwnerPort.kt
+++ src/main/kotlin/com/mad/jellomarkserver/owner/port/driven/OwnerPort.kt
@@ -3,7 +3,10 @@ package com.mad.jellomarkserver.owner.port.driven
 import com.mad.jellomarkserver.owner.core.domain.model.Owner
 import com.mad.jellomarkserver.owner.core.domain.model.OwnerEmail
 
+import com.mad.jellomarkserver.owner.core.domain.model.OwnerId
+
 interface OwnerPort {
     fun save(owner: Owner): Owner
     fun findByEmail(email: OwnerEmail): Owner?
+    fun delete(id: OwnerId)
 }
