6606474e77e11b252cede7f89ced75d18d268805
maaaaaaaaad <integral5137@gmail.com>
2026-04-19 20:58:07 +0900

    add: 샵 삭제 시 리뷰/즐겨찾기/예약/시술 cascade 삭제

--- src/main/kotlin/com/mad/jellomarkserver/beautishop/core/application/DeleteBeautishopUseCaseImpl.kt
+++ src/main/kotlin/com/mad/jellomarkserver/beautishop/core/application/DeleteBeautishopUseCaseImpl.kt
@@ -6,7 +6,11 @@ import com.mad.jellomarkserver.beautishop.core.domain.model.ShopId
 import com.mad.jellomarkserver.beautishop.port.driven.BeautishopPort
 import com.mad.jellomarkserver.beautishop.port.driving.DeleteBeautishopCommand
 import com.mad.jellomarkserver.beautishop.port.driving.DeleteBeautishopUseCase
+import com.mad.jellomarkserver.favorite.port.driven.FavoritePort
 import com.mad.jellomarkserver.owner.core.domain.model.OwnerId
+import com.mad.jellomarkserver.reservation.port.driven.ReservationPort
+import com.mad.jellomarkserver.review.port.driven.ShopReviewPort
+import com.mad.jellomarkserver.treatment.port.driven.TreatmentPort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Transactional
@@ -14,7 +18,11 @@ import java.util.*
 
 @Service
 class DeleteBeautishopUseCaseImpl(
-    private val beautishopPort: BeautishopPort
+    private val beautishopPort: BeautishopPort,
+    private val treatmentPort: TreatmentPort,
+    private val reservationPort: ReservationPort,
+    private val reviewPort: ShopReviewPort,
+    private val favoritePort: FavoritePort
 ) : DeleteBeautishopUseCase {
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
@@ -30,6 +38,10 @@ class DeleteBeautishopUseCaseImpl(
                 throw UnauthorizedBeautishopAccessException(existingShop.id.value.toString())
             }
 
+        reviewPort.deleteAllByShopId(shop.id)
+        favoritePort.deleteAllByShopId(shop.id)
+        reservationPort.deleteAllByShopId(shop.id)
+        treatmentPort.deleteAllByShopId(shop.id)
         beautishopPort.delete(shop.id)
     }
 }
