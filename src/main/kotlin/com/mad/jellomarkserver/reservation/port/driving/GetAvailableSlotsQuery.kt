35c492093e2b928aa4d2e689b364c767a5d28d40
maaaaaaaaad <integral5137@gmail.com>
2026-07-16 09:15:31 +0900

    refactor: GetAvailableSlotsQuery에 designerId 필드 추가

--- src/main/kotlin/com/mad/jellomarkserver/reservation/port/driving/GetAvailableSlotsQuery.kt
+++ src/main/kotlin/com/mad/jellomarkserver/reservation/port/driving/GetAvailableSlotsQuery.kt
@@ -3,5 +3,6 @@ package com.mad.jellomarkserver.reservation.port.driving
 data class GetAvailableSlotsQuery(
     val shopId: String,
     val treatmentId: String,
-    val date: String
+    val date: String,
+    val designerId: String? = null
 )
